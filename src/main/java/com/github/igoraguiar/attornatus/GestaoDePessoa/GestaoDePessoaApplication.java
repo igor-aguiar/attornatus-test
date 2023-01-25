1bc5c8b48b7708f6dda46fd392d443d8044e16e9
Igor Aguiar <desenvigor@gmail.com>
2023-01-25 11:36:51 -0300

    Initial Commit

---
+++ src/main/java/com/github/igoraguiar/attornatus/GestaoDePessoa/GestaoDePessoaApplication.java
@@ -0,0 +1,13 @@
+package com.github.igoraguiar.attornatus.GestaoDePessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDePessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDePessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/igoraguiar/attornatus/GestaoDePessoa/GestaoDePessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.igoraguiar.attornatus.GestaoDePessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDePessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
